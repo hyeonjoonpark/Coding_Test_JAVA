0021a7abe217294ee44c6cc0bfb3f87d0c1ed447
hyeonjoonpark <pjjoon1379@gmail.com>
2024-01-05 22:36:58 +0900

    순차탐색

---
+++ src/Algorithm/Search/LinearSearch/LinearSearch.java
@@ -0,0 +1,30 @@
+package Algorithm.Search.LinearSearch;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static int find(int X, int[] arr) {
+        for(int i = 0; i<arr.length; i++) {
+            if(arr[i] == X) {
+                return i; // 값을 찾으면 인덱스 반환
+            }
+        }
+        return -1; // 찾는 값이 없으면 -1 반환
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); // 배열 요소의 갯수
+        int X = sc.nextInt(); // 찾아야 하는 값
+        int[] arr = new int[N]; // 배열 선언
+
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt(); // 배열 요소 입력
+        }
+
+        int result = find(X, arr);
+
+        System.out.println("요소가 들어있는 인덱스 = " + result);
+    }
+}
---
+++ src/Algorithm/Search/LinearSearch/README.md
@@ -0,0 +1,11 @@
+# Linear Search(순차 탐색)
+
+순차탐색은 원하는 키값을 가지는 요소를 만날 때까지 탐색하는 알고리즘이다.
+
+### 순차탐색의 종료조건
+#### 1. 배열을 모두 탐색하였지만 값을 찾지 못한 경우 (-1 반환)
+#### 2. 값은 찾은 경우 (값의 인덱스를 반환)
+
+![스크린샷 2024-01-05 오후 10.25.29.png](..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2Fvar%2Ffolders%2Fg7%2Fxw2wx01d1pz9cbb4xyvqgczr0000gn%2FT%2FTemporaryItems%2FNSIRD_screencaptureui_usqIsh%2F%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7%202024-01-05%20%EC%98%A4%ED%9B%84%2010.25.29.png)
+
+![스크린샷 2024-01-05 오후 10.36.14.png](..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2Fvar%2Ffolders%2Fg7%2Fxw2wx01d1pz9cbb4xyvqgczr0000gn%2FT%2FTemporaryItems%2FNSIRD_screencaptureui_6gx8vd%2F%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7%202024-01-05%20%EC%98%A4%ED%9B%84%2010.36.14.png)
