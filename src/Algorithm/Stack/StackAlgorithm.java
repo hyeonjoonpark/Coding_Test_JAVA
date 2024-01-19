3ef77e2e2c5b40e06bb019df8e6b3e2cebd2cc8d
hyeonjoonpark <pjjoon1379@gmail.com>
2024-01-19 21:32:33 +0900

    STACK

---
+++ src/Algorithm/Stack/StackAlgorithm.java
@@ -0,0 +1,26 @@
+package Algorithm.Stack;
+
+import java.util.Stack;
+
+public class StackAlgorithm {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>(); // Stack 생성
+
+        // Stack에 값 넣기 push()
+        // 1부터 5까지 숫자 넣기
+        for (int i=1; i<=5; i++) {
+            stack.push(i);
+        }
+
+        // Stack이 비어있는지 확인 isEmpty()
+        boolean result = stack.isEmpty();
+
+        // 만약 Stack이 비어있지 않으면
+        if(!result) {
+            // Stack에서 값 제거 pop()
+            stack.pop(); // 마지막에 들어간 값인 5제거 (Last In First Out)
+        }
+
+        System.out.println(stack); // [1, 2, 3, 4]
+    }
+}
