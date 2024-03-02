82724f507825717fb3b7bbecf3a83c00c9bda489
hyeonjoonpark <pjjoon1379@gmail.com>
2024-03-02 10:54:37 +0900

    BinarySearch

--- src/Algorithm/Search/BinarySearch/BinarySearch.java
+++ src/Algorithm/Search/BinarySearch/BinarySearch.java
@@ -4,43 +4,46 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class BinarySearch {
-    public static int binarySearch(int[] arr, int key, int left, int right) {
-        if (left > right) {
-            return -1;
-        }
+  public static int binarySearch(int[] arr, int key) {
+      int pl = 0;
+      int pr = arr.length - 1;
 
-        int mid = left + (right - left) / 2; // 오버플로 방지
+      do {
+        int pc = (pl + pr) / 2;
 
-        if (arr[mid] < key) {
-            return binarySearch(arr, key, mid + 1, right); // 오른쪽 탐색
-        } else if (arr[mid] > key) {
-            return binarySearch(arr, key, left, mid - 1); // 왼쪽 탐색
-        } else {
-            return mid; // 찾은 경우, 인덱스 반환
+        if(arr[pc] == key) return pc;
+        else if(key > arr[pc]) {
+          pl = pc + 1;
         }
+        else {
+          pr = pc -1;
+        }
+      } while(pl <= pr);
+
+      return -1;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt(); // 배열 요소 갯수 입력
+    int key = sc.nextInt(); // 검색할 값
+    int[] array = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      array[i] = sc.nextInt();
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    Arrays.sort(array); // BinarySearch는 무조건 정렬되어있어야 함
 
-        int N = sc.nextInt(); // 배열 요소 갯수 입력
-        int key = sc.nextInt(); // 검색할 값
-        int[] array = new int[N];
+    int result = binarySearch(array, key);
 
-        for (int i = 0; i < N; i++) {
-            array[i] = sc.nextInt();
-        }
-
-        Arrays.sort(array); // BinarySearch는 무조건 정렬되어있어야 함
-
-        int result = binarySearch(array, key, 0, N - 1);
-
-        if (result == -1) {
-            System.out.println("찾는 값이 존재하지 않습니다");
-        } else {
-            System.out.println(result); // 찾는 값의 인덱스 출력
-        }
-
-        sc.close();
+    if (result == -1) {
+      System.out.println("찾는 값이 존재하지 않습니다");
+    } else {
+      System.out.println(result); // 찾는 값의 인덱스 출력
     }
+
+    sc.close();
+  }
 }
