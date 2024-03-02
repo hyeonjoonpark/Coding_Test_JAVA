feb9a4bf7090047861d24d0778fd8a3eb797d5f7
hyeonjoonpark <pjjoon1379@gmail.com>
2024-03-02 10:43:26 +0900

    Linear Search

--- src/Algorithm/Search/LinearSearch/LinearSearch.java
+++ src/Algorithm/Search/LinearSearch/LinearSearch.java
@@ -1,16 +1,7 @@
 package Algorithm.Search.LinearSearch;
 
 import java.util.Scanner;
-
 public class LinearSearch {
-    public static int find(int X, int[] arr) {
-        for(int i = 0; i<arr.length; i++) {
-            if(arr[i] == X) {
-                return i; // 값을 찾으면 인덱스 반환
-            }
-        }
-        return -1; // 찾는 값이 없으면 -1 반환
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -23,8 +14,19 @@ public class LinearSearch {
             arr[i] = sc.nextInt(); // 배열 요소 입력
         }
 
-        int result = find(X, arr);
+        int result = Search.find(X, arr);
 
         System.out.println("요소가 들어있는 인덱스 = " + result);
     }
+}
+
+class Search {
+  static int find(int X, int[] arr) {
+    for(int i = 0; i<arr.length; i++) {
+      if(arr[i] == X) {
+        return i; // 값을 찾으면 인덱스 반환
+      }
+    }
+    return -1; // 찾는 값이 없으면 -1 반환
+  }
 }
