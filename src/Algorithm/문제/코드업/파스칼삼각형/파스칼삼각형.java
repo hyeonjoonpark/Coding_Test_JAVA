6e7ef382cd8d53bbda586332e1ba9869e422adef
hyeonjoonpark <pjjoon1379@gmail.com>
2024-03-18 17:23:33 +0900

    coding test

--- src/Algorithm/문제/코드업/파스칼삼각형/파스칼삼각형.java
+++ src/Algorithm/문제/코드업/파스칼삼각형/파스칼삼각형.java
@@ -6,7 +6,7 @@ public class 파스칼삼각형 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    int[][] arr = new int[n][n];
+    long[][] arr = new long[n][n];
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j <= i; j++) {
@@ -20,7 +20,7 @@ public class 파스칼삼각형 {
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j <= i; j++) {
-        System.out.print(arr[i][j]);
+        System.out.print(arr[i][j] + " ");
       }
       System.out.println();
     }
